f0e0e8d29c500534f0f02227e2ec19a5bf7a6b4f
Dhara Patel <dhara.patel95@hotmail.co.uk>
2020-11-16 11:59:41 +0000

    Add onClick event and 'buy' function, to return a pop-up message when 'Buy' button is clicked

--- script.jsx
+++ script.jsx
@@ -1,12 +1,16 @@
 var Product = React.createClass({
-    render: function() {
-        return (
-          <div>
-            <p>Android - $199</p> 
-            <button> Buy</button>
-          </div>
-        );
-    }
+  buy: function() {
+    alert("You bought the Android mobile");
+  }, 
+
+  render: function() {
+    return (
+      <div>
+        <p>Android - $199</p> 
+        <button onClick={this.buy}> Buy</button>
+      </div>
+    );
+  }
 });
 
 React.render(<Product/>, document.getElementById("root"));
