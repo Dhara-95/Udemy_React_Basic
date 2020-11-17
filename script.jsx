0186cf4bc70bd0c0d21d935c7ade85977157c062
Dhara Patel <dhara.patel95@hotmail.co.uk>
2020-11-17 14:45:39 +0000

    Pass data between Child components

--- script.jsx
+++ script.jsx
@@ -4,10 +4,11 @@ var Product = React.createClass({
   },
   buy: function() {
     this.setState({qty: this.state.qty + 1});
+    this.props.handleTotal(this.props.price);
   }, 
   
   show: function() {
-
+    this.props.handleShow(this.props.name);
   },
 
   render: function() {
@@ -27,13 +28,21 @@ var Total = React.createClass({
   render: function() {
     return (
       <div>
-        <h3>Total Cash: </h3>
+        <h3>Total Cash: ${this.props.total}</h3>
       </div>
     );
   }  
 });
 
 var ProductList = React.createClass({
+  getInitialState: function() {
+    return {total: 0};
+  },
+
+  calculateTotal: function(price) {
+    this.setState({total: this.state.total + price});
+  },
+
   showProduct: function(name) {
     alert("You selected " + name);   
   },
@@ -41,10 +50,16 @@ var ProductList = React.createClass({
   render: function() {
     return (
       <div>
-        <Product name="Android" price="121" handleShow={this.showProduct}/>
-        <Product name="Apple" price="123" handleShow={this.showProduct}/>
-        <Product name="Nokia" price="65" handleShow={this.showProduct}/>
-        <Total/>
+        <Product name="Android" price={121} 
+          handleShow={this.showProduct} 
+          handleTotal={this.calculateTotal}/>
+        <Product name="Apple" price={123} 
+          handleShow={this.showProduct} 
+          handleTotal={this.calculateTotal}/>
+        <Product name="Nokia" price={65} 
+          handleShow={this.showProduct} 
+          handleTotal={this.calculateTotal}/>
+        <Total total={this.state.total}/>
       </div>
     );
   }
